fd1d4d821f430b4002058c744a50c1c4eda510bc
Rajab M <rajab1.masri@gmail.com>
2024-09-19 14:46:48 +0300

    setup database and models

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 from flask import Flask, render_template
+from models import db, Item
+from init_db import item_model
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
+db.init_app(app)
 
-@app.route('/')
-@app.route('/home')
+with app.app_context():
+    db.create_all()
+    if not Item.query.first():
+        item_model(app)
+
+
+@app.route("/")
+@app.route("/home")
 def home_page():
-    return render_template('home.html.jinja')
+    return render_template("home.html.jinja")
 
-@app.route('/market')
+
+@app.route("/market")
 def market_page():
-    items = [
-        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
-    ]
-    return render_template('market.html.jinja', items=items)
-
+    items = Item.query.all()
+    return render_template("market.html.jinja", items=items)
---
+++ init_db.py
@@ -0,0 +1,32 @@
+from models import db, Item
+
+
+def item_model(app):
+    with app.app_context():
+        item1 = Item(
+            name="IPhone 10",
+            price=500,
+            barcode="893212299897",
+            description="This is Phone",
+        )
+        item2 = Item(
+            name="Laptop",
+            price=900,
+            barcode="123985473165",
+            description="This is Laptop",
+        )
+        item3 = Item(
+            name="Keyboard",
+            price=20.5,
+            barcode="231985128446",
+            description="This is Keyboard",
+        )
+
+        db.session.add_all([item1, item2, item3])
+        db.session.commit()
+
+        items = Item.query.all()
+        for item in items:
+            print(
+                f"Item Name: {item.name}, Price: {item.price}, Barcode: {item.barcode}, Description: {item.description}"
+            )
