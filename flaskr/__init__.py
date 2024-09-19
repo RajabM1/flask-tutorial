4c961807c972965bcc702332275a842245d5d421
Rajab M <rajab1.masri@gmail.com>
2024-09-20 00:47:59 +0300

    add register form page

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
+app.config["SECRET_KEY"] = "4a90e28d582d5cce03ec50a5"
 db = SQLAlchemy(app)
 
 
--- flaskr/routes.py
+++ flaskr/routes.py
@@ -1,6 +1,7 @@
 from flaskr import app
 from flask import render_template
 from flaskr.models.item import Item
+from flaskr.forms import RegisterForm
 
 
 @app.route("/")
@@ -13,3 +14,9 @@ def home_page():
 def market_page():
     items = Item.query.all()
     return render_template("market.html.jinja", items=items)
+
+
+@app.route("/register")
+def register_page():
+    form = RegisterForm()
+    return render_template("register.html.jinja", form=form)
