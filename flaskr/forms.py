a37541d2dc5465fb7d4a3cb4ae5125e12261f82a
Rajab M <rajab1.masri@gmail.com>
2024-09-20 22:29:54 +0300

    Added a form validation
    Added custom validation to check existing username and email in registration
    
    Added wtform validation to the form input
    
    Added a flash messages to give a user a feedback during registration

--- flaskr/forms.py
+++ flaskr/forms.py
@@ -1,10 +1,33 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, EqualTo, DataRequired, Email, ValidationError
+from flaskr.models.user import User
 
 
 class RegisterForm(FlaskForm):
-    username = StringField(label="User Name")
-    email = StringField(label="Email")
-    password1 = PasswordField(label="Password")
-    password2 = PasswordField(label="Confirm Password")
+
+    def validate_username(self, username_to_validate):
+        user = User.query.filter_by(username=username_to_validate.data).first()
+        if user:
+            raise ValidationError(
+                "User Name already exists! Please try a different User Name"
+            )
+
+    def validate_email(self, email_to_validate):
+        user = User.query.filter_by(email=email_to_validate.data).first
+        if user:
+            raise ValidationError(
+                "Email address already exist! Please try a different Email Address"
+            )
+
+    username = StringField(
+        label="User Name", validators=[Length(min=2, max=15), DataRequired()]
+    )
+    email = StringField(label="Email", validators=[Email(), DataRequired()])
+    password1 = PasswordField(
+        label="Password", validators=[Length(min=6), DataRequired()]
+    )
+    password2 = PasswordField(
+        label="Confirm Password", validators=[EqualTo("password1"), DataRequired()]
+    )
     submit = SubmitField(label="Create Account")
--- flaskr/routes.py
+++ flaskr/routes.py
@@ -1,5 +1,5 @@
-from flaskr import app
-from flask import render_template
+from flaskr import app, db
+from flask import render_template, redirect, url_for, flash
 from flaskr.models.item import Item
 from flaskr.models.user import User
 from flaskr.forms import RegisterForm
@@ -23,7 +23,20 @@ def users_page():
     return render_template("users.html.jinja", users=users)
 
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(
+            username=form.username.data,
+            email=form.email.data,
+            password=form.password1.data,
+        )
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for("users_page"))
+    if form.errors != {}:
+        for err_msg in form.errors.values():
+            flash(f"{err_msg}", category="danger")
+
     return render_template("register.html.jinja", form=form)
