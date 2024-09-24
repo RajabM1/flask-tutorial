00ac772e31ced5620f87f3d3e5bcfb0dd48fd737
Rajab M <rajab1.masri@gmail.com>
2024-09-24 16:19:37 +0300

    update auth_schema and item_schema files
    solve a spelling error
    
    use @validates instead of @validates_schema

--- flaskr/schemas/auth_schema.py
+++ flaskr/schemas/auth_schema.py
@@ -1,24 +1,22 @@
 from flaskr import ma
 from flaskr.models.user import User
-from flaskr.schemas import fields, Length, validates_schema, ValidationError
+from flaskr.schemas import fields, Length, ValidationError, validates
 
 
 class AuthSchema(ma.SQLAlchemyAutoSchema):
-    username = fields.Str(required=True, valdiate=Length(min=2, max=30))
-    email = fields.Str(required=True, valdiate=Length(min=5, max=60))
-    password_hash = fields.Str(required=True, valdiate=Length(min=6, max=60))
+    username = fields.Str(required=True, validate=Length(min=2, max=30))
+    email = fields.Str(required=True, validate=Length(min=5, max=60))
+    password_hash = fields.Str(required=True, validate=Length(min=6, max=60))
     budget = fields.Integer(required=False)
 
-    @validates_schema
-    def validate_username(self, data, **kwargs):
-        username_to_validate = data.get("username")
-        if User.query.filter_by(username=username_to_validate).first():
+    @validates("username")
+    def validates_username(self, username):
+        if User.query.filter(User.username == username).first():
             raise ValidationError("Name already exists! Please try a different Name")
 
-    @validates_schema
-    def validate_email(self, data, **kwargs):
-        email_to_validate = data.get("email")
-        if User.query.filter_by(email=email_to_validate).first():
+    @validates("email")
+    def validates_email(self, email):
+        if User.query.filter(User.email == email).first():
             raise ValidationError("Email already exists! Please try a different Email")
 
     class Meta:
--- flaskr/schemas/item_schema.py
+++ flaskr/schemas/item_schema.py
@@ -1,22 +1,20 @@
 from flaskr import ma
 from flaskr.models.item import Item
-from flaskr.schemas import fields, Length, validates_schema, ValidationError
+from flaskr.schemas import fields, Length, validates, ValidationError
 
 
 class ItemSchema(ma.SQLAlchemyAutoSchema):
-    name = fields.Str(required=True, valdiate=Length(min=2, max=30))
+    name = fields.Str(required=True, validate=Length(min=2, max=30))
     price = fields.Float(required=True)
-    barcode = fields.Str(required=True, valdiate=Length(min=6, max=12))
-    description = fields.Str(required=True, valdiate=Length(min=2, max=1024))
+    barcode = fields.Str(required=True, validate=Length(min=6, max=12))
+    description = fields.Str(required=True, validate=Length(min=2, max=1024))
     owner = fields.Integer(required=False)
 
-    @validates_schema
-    def validate_name(self, data, **kwargs):
-        name_to_validate = data.get("name")
-        if Item.query.filter_by(name=name_to_validate).first():
+    @validates("name")
+    def validates_name(self, name):
+        if Item.query.filter(Item.name == name).first():
             raise ValidationError("Name already exists! Please try a different Name")
 
     class Meta:
         model = Item
-        exclude = ["id"]
         load_instance = True
