6cbaa1a70b50abbe1baa48c4769578f2958d2992
Rajab M <rajab1.masri@gmail.com>
2024-09-30 17:40:04 +0300

    Setup basic layout
    Implement Footer and NavBar layout component.
    
    Implement the root page that contain the basic layout, to make other pages inherit it .
    
    Create routes.tsx to define the app routes.
    
    Create routes\index.tsx to createBrowserRouter.

---
+++ frontend/src/components/layout/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-white text-center py-3 mt-auto">
+      <div className="container">
+        <span>&copy; 2024 FourMed. All rights reserved.</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import Root from "./Root";
+
+const Home = () => {
+    return (
+        <Root>
+            <div>Home</div>
+        </Root>
+    );
+};
+
+export default Home;
