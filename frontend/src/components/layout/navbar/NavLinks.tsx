0939a06d6543082b64b6ca679e7e4de9e603560d
Rajab M <rajab1.masri@gmail.com>
2024-10-12 22:12:41 +0300

    Update Navbar

--- frontend/src/components/layout/navbar/NavLinks.tsx
+++ frontend/src/components/layout/navbar/NavLinks.tsx
@@ -1,19 +1,25 @@
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom"
+import { getUserRole } from "../../../utils/jwtHelpers";
 
 const NavLinks = () => {
     const { t } = useTranslation('root');
+    const role = getUserRole();
+
+    const canAccessMarket = (role: string) => role !== 'guest';
+    const canAccessUsers = (role: string) => role === 'admin';
+
     return (
         <ul className="navbar-nav me-auto">
             <li className="nav-item">
-                <Link className="nav-link active" to="/">{t('nav_links.home')}</Link>
+                <Link className="nav-link" to="/">{t('nav_links.home') }</Link>
             </li>
-            <li className="nav-item">
+            {canAccessMarket(role) && <li className="nav-item">
                 <Link className="nav-link" to="/market">{t('nav_links.market')}</Link>
-            </li>
-            <li className="nav-item">
+            </li>}
+            {canAccessUsers(role) && <li className="nav-item">
                 <Link className="nav-link" to="/users">{t('nav_links.users')}</Link>
-            </li>
+            </li>}
         </ul>
     )
 }
